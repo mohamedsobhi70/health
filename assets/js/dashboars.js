0306d1ce3a014013717a714d93a28410fc930f17
mohammedsobhi70 <sobhi7070@gmail.com>
2022-08-01 01:35:27 +0200

    add new

--- assets/js/dashboars.js
+++ assets/js/dashboars.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-  var chart = new CanvasJS.Chart("totalPatients", {
+  var totalpatients = new CanvasJS.Chart("totalPatients", {
     axisX: {
       lineThickness: 0,
       labelFontColor: "#8898AA",
@@ -30,7 +30,40 @@ window.onload = function () {
     ],
   });
 
-  chart.render();
+  totalpatients.render();
+  var totalVisits = new CanvasJS.Chart("totalVisits", {
+    animationEnabled: true,
+    theme: "light1", // "light1", "light2", "dark1", "dark2"
+    title: {
+      lineThickness: 0,
+      gridDashType: "longDash",
+      gridColor: "#ccc",
+    },
+    axisY: {
+      lineThickness: 0,
+      gridDashType: "longDash",
+      gridColor: "#8898AA",
+      labelFontColor: "#8898AA",
+      interval: 10000,
+    },
+    dataPointWidth: 20,
+    data: [
+      {
+        type: "column",
+        color: "#7764E4",
+        dataPoints: [
+          { label: "May", y: 60000 },
+          { label: "Jun", y: 90000 },
+          { label: "Jul", y: 30000 },
+          { label: "Aug", y: 10000 },
+          { label: "Sep", y: 60000 },
+          { label: "Oct", y: 115000 },
+        ],
+      },
+    ],
+  });
+
+  totalVisits.render();
 
   var chartvisits = new CanvasJS.Chart("visits", {
     animationEnabled: true,
@@ -47,7 +80,7 @@ window.onload = function () {
       labelFontColor: "#8898AA",
       interval: 10,
     },
-    dataPointWidth:10,
+    dataPointWidth: 10,
     data: [
       {
         type: "column",
@@ -64,4 +97,33 @@ window.onload = function () {
     ],
   });
   chartvisits.render();
+
+  //referals
+
+  var chart = new CanvasJS.Chart("referals", {
+    animationEnabled: true,
+    title: {
+      text: "1065 visits",
+      fontSize: 18,
+      horizontalAlign: "center",
+      verticalAlign: "center",
+    },
+    data: [
+      {
+        type: "doughnut",
+        startAngle: 100,
+        //innerRadius: 60,
+        indexLabelFontSize: 7,
+        // indexLabel: "",
+        toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+        dataPoints: [
+          { y: 350, label: "Social Media", color: "#2DCE98" },
+          { y: 501, label: "Friends and Family", color: "#E54D5B" },
+          { y: 76, label: "Doctors", color: "#11CDEF" },
+          { y: 129, label: "Lab", color: "#FEB969" },
+        ],
+      },
+    ],
+  });
+  chart.render();
 };
