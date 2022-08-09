d4e0f8d6a087db4a2d6a5efa4cb86c0288739afd
mohammedsobhi70 <sobhi7070@gmail.com>
2022-08-09 15:44:49 +0200

    add day calendar drag drop option

--- assets/js/visits.js
+++ assets/js/visits.js
@@ -32,3 +32,25 @@ patientItemsinput.forEach((item) =>
     }
   })
 );
+
+// drag drop 
+
+// let waitingLists = document.querySelectorAll(".waiting-item");
+// console.log(waitingLists);
+// waitingLists.forEach(list =>{
+  
+
+// } );
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
