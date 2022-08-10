c0948e0e000ff9bc6dced3fb32a01399b11451e5
mohammedsobhi70 <sobhi7070@gmail.com>
2022-08-10 11:08:21 +0200

    add week days

--- assets/js/weeks.js
+++ assets/js/weeks.js
@@ -1,7 +1,53 @@
-let date = new Date(2021, 0, 2);
-let date2 =  
-function calendarRender() {
-  let daySection = document.querySelector(".week-date");
+// function calendarRender() {
+//   let date = new Date(2021, 0, 2);
+//   let first = date.getDate() - date.getDay();
+//   let firstday = new Date(date.setDate(first));
+//   console.log(firstday);
+//   let daySection = document.querySelector(".week-date");
+
+//   daySection.innerHTML = ` ${months[firstday.getMonth()]} ${firstday.getDate()} -  ${months[date.getMonth()]} ${date.getDate()}  `;
+// ;
+// }
+
+// calendarRender();
+// document.querySelector(".prev").addEventListener("click", () => {
+//   date.setDate(date.getDate() - 1);
+//   calendarRender();
+// });
+// document.querySelector(".next").addEventListener("click", () => {
+//   date.setDate(date.getDate() + 1);
+//   calendarRender();
+// });
+// console.log(document.querySelector(".next"));
+
+// drag drop
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
+
+//
+const today = new Date(2021, 0, 2);
+
+function getFirstDayOfWeek(d) {
+  const date = new Date(d);
+  const day = date.getDay();
+  const diff = date.getDate() - day + (day === 0 ? -7 : 1);
+  return new Date(date.setDate(diff));
+}
+let firstDay = getFirstDayOfWeek(today);
+let lastDay = new Date(firstDay);
+lastDay.setDate(lastDay.getDate() + 6);
+function calenderRender() {
   const months = [
     "Jan",
     "Feb",
@@ -16,16 +62,22 @@ function calendarRender() {
     "Nov",
     "Dec",
   ];
-  daySection.innerHTML = ` ${months[date.getMonth()]} ${date.getDate() } -  `;
+  let daySection = document.querySelector(".week-date");
+  daySection.innerHTML = ` ${
+    months[firstDay.getMonth()]
+  } ${firstDay.getDate()} -  ${
+    months[lastDay.getMonth()]
+  } ${lastDay.getDate()}  `;
 }
 
-calendarRender();
+calenderRender();
 document.querySelector(".prev").addEventListener("click", () => {
-  date.setDate(date.getDate() - 1);
-  calendarRender();
-});
-document.querySelector(".next").addEventListener("click", () => {
-  date.setDate(date.getDate() + 1);
-  calendarRender();
-});
-console.log(document.querySelector(".next"));
+    firstDay.setDate(firstDay.getDate() - 7);
+    lastDay.setDate(lastDay.getDate() - 7);
+    calenderRender();
+  });
+  document.querySelector(".next").addEventListener("click", () => {
+    firstDay.setDate(firstDay.getDate() + 7);
+    lastDay.setDate(lastDay.getDate() + 7);
+    calenderRender();
+  });
